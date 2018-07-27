5b1e00c1b9c9645c17eb8135d2f2d13eb48f7e13
Andrew Swenson <swena56@gmail.com>
2018-07-26 18:16:24 -0700

    add windows doskey

---
+++ web/selenium/install/selenium_standalone.js
@@ -0,0 +1,5 @@
+//curl -O http://selenium-release.storage.googleapis.com/3.5/selenium-server-standalone-3.5.3.jar
+
+npm install -g selenium-standalone phantomjs-prebuilt
+selenium-standalone install
+selenium-standalone start
---
+++ windows/sublime_doskey.bat
@@ -0,0 +1,3 @@
+doskey subl="C:\Program Files\Sublime Text 3\sublime_text.exe" $*
+
+doskey ls=dir 
