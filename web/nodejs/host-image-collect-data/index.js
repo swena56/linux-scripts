d370101b536b0510ba98456c384e567ea80b5156
Andrew Swenson <Andrew.Swenson@bestbuy.com>
2018-12-05 21:52:36 -0600

    host data collect

--- web/nodejs/host-image-collect-data/index.js
+++ web/nodejs/host-image-collect-data/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const request = require('request');
 const fs = require('fs');
 const util = require('./util');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
+const locations = 'locations.json';
+
+function reg(ip) {
+    var re = /-(\d+)/ig
+    var arr = [];
+    while(digit = re.exec(ip)) arr.push(digit[1]);
+    arr = arr.slice(0,4);
+
+    return arr;
+}
 
 app.get('/', function(req, res) {
 
@@ -11,7 +21,16 @@ app.get('/', function(req, res) {
     res.writeHead(200, {'Content-Type': 'image/png' });
     res.end(img, 'binary');
 
-    util.storeIp(req.ip);
+    request.get(`http://ipinfo.io/${reg(reg.ip).join('.')}`, {json: true}, (e, r) => {
+			var data = r.body;
+			if( data ){
+				data['timestamp'] = (new Date).toISOString();
+				console.log(data);
+				fs.appendFile(locations, JSON.stringify(data) +"\n", function(err, data){
+				    if (err) console.log(err);
+				});
+			}
+	});
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- web/nodejs/host-image-collect-data/util.js
+++ web/nodejs/host-image-collect-data/util.js
@@ -3,10 +3,13 @@ const fs = require('fs');
 const locations = 'locations.json';
 
 module.exports = {
+	storeInfo: function(request){
+
+	},
+
 	storeIp: function(ip){
 		if( !ip ) throw Error('Need ip for param 1');
 
-		console.log(ip);
 		request.get(`http://ipinfo.io/${ip}`, {json: true}, (e, r) => {
 			var data = r.body;
 			console.log(data);
@@ -18,5 +21,43 @@ module.exports = {
 				});
 			}
 		});
+	},
+	writeToDisk: function(data){
+		let store_str = JSON.stringify(data) || undefined;
+		if( data && store_str ){
+			fs.appendFile(locations, store_str +"\n", function(err, data){
+			    if (err) console.log(err);
+			    console.log(`Successfully Written to File: ${store_str}`);
+			});
+		}
+	},
+	detectBrowser: function(headers){
+		let ua = headers['user-agent'];
+		let $ = {};
+
+		if (/mobile/i.test(ua))
+		    $.Mobile = true;
+
+		if (/like Mac OS X/.test(ua)) {
+		    $.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
+		    $.iPhone = /iPhone/.test(ua);
+		    $.iPad = /iPad/.test(ua);
+		}
+
+		if (/Android/.test(ua))
+		    $.Android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
+
+		if (/webOS\//.test(ua))
+		    $.webOS = /webOS\/([0-9\.]+)[\);]/.exec(ua)[1];
+
+		if (/(Intel|PPC) Mac OS X/.test(ua))
+		    $.Mac = /(Intel|PPC) Mac OS X ?([0-9\._]*)[\)\;]/.exec(ua)[2].replace(/_/g, '.') || true;
+
+		if (/Windows NT/.test(ua))
+		    $.Windows = /Windows NT ([0-9\._]+)[\);]/.exec(ua)[1];
+
+		console.log($);
+
+		return $;
 	},
 }
