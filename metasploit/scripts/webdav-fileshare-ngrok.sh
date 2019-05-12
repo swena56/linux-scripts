5c59915e19ac4733f47bc8ba19f027f7f32f4efd
root <root@kali>
2019-05-12 01:25:30 -0500

    webdav filesharing

--- metasploit/scripts/webdav-fileshare-ngrok.sh
+++ metasploit/scripts/webdav-fileshare-ngrok.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
+# files can be semt via curl:  curl https://<ID>.ngrok.io --upload-file file-name.txt
+FOLDER=$1
+if [ ! -d "$FOLDER" ]; then
+	echo "Need file directory"
+	mkdir -p /tmp/webdav/share	
+	read -p "FOLDER [/tmp/webdav/share]: " FOLDER; FOLDER=${FOLDER:-'/tmp/webdav/share'}
+	[ ! -d "$FOLDER" ] && echo "Invalid directory" && exit
+fi
 [ -z "$(which wsgidav)" ] && pip install wsgidav cheroot
 /etc/init.d/apache2 stop > /dev/null 2>&1 &
-wsgidav --host=0.0.0.0 --port=80 --root "$(pwd)" --auth=anonymous > /dev/null 2>&1 &
+
+pkill wsgidav
+wsgidav --host=0.0.0.0 --port=80 --root "$FOLDER" --auth=anonymous > /dev/null 2>&1 &
+#wsgidav --host=0.0.0.0 --port=80 --root "$1	" --auth=nt > /dev/null 2>&1 &
 pkill ngrok
 ngrok http 80
 pkill wsgidav
---
+++ misc/start-simple-ftp-server.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+FTP_PORT = 2121
+FTP_USER = "afs"
+FTP_PASSWORD = "afs"
+FTP_DIRECTORY = "/tmp"
+
+
+def main():
+    authorizer = DummyAuthorizer()
+    authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_DIRECTORY, perm='elradfmw')
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+    handler.banner = "Python's afs FTP Server"
+
+    address = ('', FTP_PORT)
+    server = FTPServer(address, handler)
+
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
