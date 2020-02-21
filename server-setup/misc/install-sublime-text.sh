01aa1f46bfb0ea5e05673d1f7a8973b852222bb2
afs <afs@DESKTOP-3IDM0TC>
2020-02-20 20:44:12 -0600

    add sublime

--- server-setup/misc/install-sublime-text.sh
+++ server-setup/misc/install-sublime-text.sh
@@ -4,3 +4,4 @@ sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text -y
+
